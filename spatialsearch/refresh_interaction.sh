841ab95d91715019a1e85f4e9d42310c51869967
Jorrit Poelen <jhpoelen@gmail.com>
2013-12-12 12:02:38 -0800

    add interaction refresh script

---
+++ spatialsearch/refresh_interaction.sh
@@ -0,0 +1,3 @@
+#refreshes interaction.json
+
+curl "http://trophicgraph.com:8080/interaction?type=json.v2&nw_lat=19.311142969053392&nw_lng=-67.48169018749996&se_lat=17.39257971605542&se_lng=-65.19653315624998" > interaction.json
