8cdefad964ca0b587ffe79f425c3a0b0eed78530
Jorrit Poelen <jhpoelen@gmail.com>
2014-04-18 16:54:04 -0700

    add link to new API page

--- menu.js
+++ menu.js
@@ -15,7 +15,7 @@ var buildMenu = function() {
         return menu;
     }
     appendMenuItem('about', '/about.html');
-    appendMenuItem('api', 'https://github.com/jhpoelen/eol-globi-data/wiki/rest');
+    appendMenuItem('api', 'https://github.com/jhpoelen/eol-globi-data/wiki/api');
     appendMenuItem('blog', 'http://globalbioticinteractions.wordpress.com');
    	appendMenuItem('wiki', 'http://github.com/jhpoelen/eol-globi-data/wiki');
     appendMenuItem('feedback', '/feedback.html');
