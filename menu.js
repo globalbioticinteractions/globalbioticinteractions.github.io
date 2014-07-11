5db66cd3673419f870dedb78d2dabb2a026ac89a
Jorrit Poelen <jhpoelen@gmail.com>
2014-07-11 12:30:41 -0700

    added data menu item

--- menu.js
+++ menu.js
@@ -25,6 +25,7 @@ var buildMenu = function() {
     appendMenuItem('Global Biotic Interactions', '/index.html');
     appendMenuItem('About', '/about.html');
     appendMenuItem('API', 'https://github.com/jhpoelen/eol-globi-data/wiki/api');
+    appendMenuItem('Data', 'https://github.com/jhpoelen/eol-globi-data/wiki#accessing-species-interaction-data');
     appendMenuItem('Blog', 'http://globalbioticinteractions.wordpress.com');
    	appendMenuItem('Wiki', 'http://github.com/jhpoelen/eol-globi-data/wiki');
     appendMenuItem('Feedback', '/feedback.html');
