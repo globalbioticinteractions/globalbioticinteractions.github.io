5fee3507ecfcfc18a64ee6baadcd690f64d63467
Jorrit Poelen <jhpoelen@gmail.com>
2015-08-08 13:45:20 -0700

    remove manual test files

--- browse/refresh_interaction.sh
+++
@@ -1,3 +0,0 @@
-#refreshes interaction.json
-
-curl "http://trophicgraph.com/interaction?type=json.v2&nw_lat=19.311142969053392&nw_lng=-67.48169018749996&se_lat=17.39257971605542&se_lng=-65.19653315624998" > interaction.json
