ab92582cd086172bc2c1f4abb2c64012e0f6dd39
Jorrit Poelen <jhpoelen@gmail.com>
2015-08-10 17:49:28 -0700

    use Spinner npm depedency

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var jquery = require('jquery');
 require('jquery-ui');
 
 module.exports = {
+    Spinner: require('spin.js'),
     bundle: require('globi-bundle'),
     hairball: require('globi-hairball'),
     wheel: require('globi-wheel'),
