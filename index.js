0f8e4b927cffad717f993f4dbed3c3076fbebf20
Jorrit Poelen <jhpoelen@gmail.com>
2015-08-10 18:22:27 -0700

    more refactoring

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var jquery = require('jquery');
 require('jquery-ui');
+var MarkerClusterer = require('node-js-marker-clusterer');
 
 module.exports = {
-    Spinner: require('spin.js'),
     bundle: require('globi-bundle'),
+    Spinner: require('spin.js'),
     hairball: require('globi-hairball'),
     wheel: require('globi-wheel'),
     globi: require('globi')
-
 };
