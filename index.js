6c7046cdbb71c59480bc380fa6eac0fee84843a9
Jorrit Poelen <jhpoelen@gmail.com>
2015-08-10 17:36:44 -0700

    refactor using @coding46 suggestions

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+var jquery = require('jquery');
+require('jquery-ui');
+
 module.exports = {
     bundle: require('globi-bundle'),
     hairball: require('globi-hairball'),
-    wheel: require('globi-wheel')
+    wheel: require('globi-wheel'),
+    globi: require('globi')
+
 };
