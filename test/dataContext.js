ce3e8e44db57485aedf76a7d23b307e56f375f79
jhpoelen <jhpoelen@gmail.com>
2016-11-17 08:50:24 -0800

    use number of interactions to order results instead of number_of_studies

--- test/dataContext.js
+++ test/dataContext.js
@@ -8,7 +8,7 @@ test('add fields', function (t) {
     var context = dataContext();
     var params = {one: 'one', two: 'two'};
     t.deepEqual({
-        field: [ 'source_taxon_external_id', 'source_taxon_name', 'source_taxon_path', 'source_taxon_path_ids', 'target_taxon_external_id', 'target_taxon_name', 'target_taxon_path', 'target_taxon_path_ids', 'interaction_type', 'number_of_studies' ],
+        field: [ 'source_taxon_external_id', 'source_taxon_name', 'source_taxon_path', 'source_taxon_path_ids', 'target_taxon_external_id', 'target_taxon_name', 'target_taxon_path', 'target_taxon_path_ids', 'interaction_type', 'number_of_interactions' ],
         one: 'one',
         two: 'two' }, context.prepareParameters(params), 'initialize search parameters');
 
@@ -20,7 +20,7 @@ test('add fields and more', function (t) {
     var context = dataContext();
     var params = {sourceTaxon: ['one'], two: 'two'};
     t.deepEqual({
-        field: [ 'source_taxon_external_id', 'source_taxon_name', 'source_taxon_path', 'source_taxon_path_ids', 'target_taxon_external_id', 'target_taxon_name', 'target_taxon_path', 'target_taxon_path_ids', 'interaction_type', 'number_of_studies' ],
+        field: [ 'source_taxon_external_id', 'source_taxon_name', 'source_taxon_path', 'source_taxon_path_ids', 'target_taxon_external_id', 'target_taxon_name', 'target_taxon_path', 'target_taxon_path_ids', 'interaction_type', 'number_of_interactions' ],
         sourceTaxon: ['one'],
         two: 'two' }, context.prepareParameters(params), 'initialize pluralized search parameters');
 
