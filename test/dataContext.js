27e8d856a2805ead7e6f3e317ff263778f4ad44f
jhpoelen <jhpoelen@xs4all.nl>
2020-04-13 10:37:47 -0700

    update test

--- test/dataContext.js
+++ test/dataContext.js
@@ -7,10 +7,12 @@ test('add fields', function (t) {
     t.plan(2);
     var context = dataContext();
     var params = {one: 'one', two: 'two'};
-    t.deepEqual({
-        field: [ 'source_taxon_external_id', 'source_taxon_name', 'source_taxon_path', 'source_taxon_path_ids', 'target_taxon_external_id', 'target_taxon_name', 'target_taxon_path', 'target_taxon_path_ids', 'interaction_type', 'number_of_interactions' ],
+    t.deepEqual(
+        context.prepareParameters(params),
+        {
+        field: [ 'source_taxon_external_id', 'source_taxon_name', 'source_taxon_path', 'source_taxon_path_ids', 'target_taxon_external_id', 'target_taxon_name', 'target_taxon_path', 'target_taxon_path_ids', 'interaction_type' ],
         one: 'one',
-        two: 'two' }, context.prepareParameters(params), 'initialize search parameters');
+        two: 'two' }, 'initialize search parameters');
 
     t.notOk(params['field']);
 });
@@ -19,10 +21,11 @@ test('add fields and more', function (t) {
     t.plan(2);
     var context = dataContext();
     var params = {sourceTaxon: ['one'], two: 'two'};
-    t.deepEqual({
-        field: [ 'source_taxon_external_id', 'source_taxon_name', 'source_taxon_path', 'source_taxon_path_ids', 'target_taxon_external_id', 'target_taxon_name', 'target_taxon_path', 'target_taxon_path_ids', 'interaction_type', 'number_of_interactions' ],
+    t.deepEqual(context.prepareParameters(params), 
+        {
+        field: [ 'source_taxon_external_id', 'source_taxon_name', 'source_taxon_path', 'source_taxon_path_ids', 'target_taxon_external_id', 'target_taxon_name', 'target_taxon_path', 'target_taxon_path_ids', 'interaction_type' ],
         sourceTaxon: ['one'],
-        two: 'two' }, context.prepareParameters(params), 'initialize pluralized search parameters');
+        two: 'two' }, 'initialize pluralized search parameters');
 
     t.notOk(params['field']);
 });
