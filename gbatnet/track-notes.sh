1f3a2d52ecdecb5ab14440d5fcd7d0138a79692b
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2024-12-18 11:48:06 -0600

    add pointer to most recent tracked gbatnet notes

--- gbatnet/track-notes.sh
+++ gbatnet/track-notes.sh
@@ -4,3 +4,7 @@
 #
 
 preston track "https://docs.google.com/document/d/1yvOZVzv4hnr4E0p_0cNuISyF6c5Gd2Dvps_He9cwOBg/edit"
+
+preston cat $(preston head) |grep hasVersion | grep docx | preston cat > notes.docx
+preston cat $(preston head) |grep hasVersion | grep pdf | preston cat > notes.pdf
+
