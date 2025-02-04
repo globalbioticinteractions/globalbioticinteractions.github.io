a130d313ef1c7b9f497e4ab2c9e68536676332fe
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2025-02-04 14:21:36 -0600

    add related project list

--- euroappa/track-notes.sh
+++ euroappa/track-notes.sh
@@ -11,3 +11,15 @@ preston head\
  | grep pdf\
  | preston cat\
  > notes.pdf
+
+preston track\
+ --message "Snapshot of EuroAPPA list of related projects"\
+ "https://docs.google.com/document/d/1MDfcQESQh9abAxIhAs4gCRi88SnTP0s39bscvsREgto/edit?tab=t.bcaxfrmawmxl"
+
+preston head\
+ | preston cat\
+ | grep hasVersion\
+ | grep pdf\
+ | preston cat\
+ > related-projects.pdf
+
